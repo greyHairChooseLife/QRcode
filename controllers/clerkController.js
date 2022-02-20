d67e7fc93a1f68369cd243a669dbd734a660cbed
sy <greyhairchooselife@gmail.com>
2022-02-21 04:32:41 +0900

    장바구니에 같은 제품이 나뉘어있으면(손님이 두번 했을 수 있음) 합쳐준다.

--- controllers/clerkController.js
+++ controllers/clerkController.js
@@ -12,7 +12,14 @@ const getVisitorList = async (req, res) => {
 const readVisitor = async (req, res) => {
 	const customerId = req.body.customerId;
 	const result = await customerModel.readBasket(customerId);
-
+	for(var i=0; i<result.length; i++){				//중복된 물건이면(바코드가 같음) 수량 합쳐서 하나의 요소로 만듦
+		for(var j=i+1; j<result.length; j++){
+			if(result[i].barcode === result[j].barcode){
+				result[i].quantity += result[j].quantity;
+				result.splice(j, 1);
+			}
+		}
+	}
 	const obj = {
 		customerId: customerId,
 		inCart: result,
