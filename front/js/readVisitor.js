e5d4f4468325a78b929167cbbbffcb10bf1620df
sy <greyhairchooselife@gmail.com>
2022-02-21 04:59:05 +0900

    이벤트 타겟(장바구니에 담긴 목록들)에 input type:hidden 으로 barcode 값 숨겨놓은 것으로 바코드 동적 생성한다

--- front/js/readVisitor.js
+++ front/js/readVisitor.js
@@ -1,17 +1,20 @@
+const introBarcode = document.getElementById('introBarcode');
 const itemList = document.getElementsByClassName('list');
 
 for(var i=0; i<itemList.length; i++){
 	itemList[i].addEventListener('click', (e)=>{
-		//e.target.inner
+		e.target.style.opacity = '0.2';
+		introBarcode.style.display = 'none';
+		let barcodeKey = e.target.querySelector('input').value;
+		JsBarcode("#barcode", barcodeKey, {
+			format: "CODE128",
+			lineColor: "black",
+			width: 3,
+			height: 80,
+			fontSize: 15,
+			margin: 15,
+			marginBottom: 2,
+		});
 	})
 }
 
-JsBarcode("#barcode", "12341234", {
-	format: "CODE128",
-	lineColor: "black",
-	width: 3,
-	height: 80,
-	fontSize: 15,
-	margin: 15,
-	marginBottom: 2,
-});
